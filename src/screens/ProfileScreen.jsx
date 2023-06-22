9cafe337b71077d2b2ced13bea45f592ccdb5a55
Roshan <roshan.jha@hih7.in>
2023-06-22 12:54:00 +0530

    Update Profile Functionality

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -7,6 +7,7 @@ import FormContainer from "../components/FormContainer";
 import { toast } from "react-toastify";
 import Loader from "../components/Loader";
 import { setCredentials } from "../slice/authSlice";
+import { useUpdateUserMutation } from "../slice/userApiSlice";
 
 const ProfileScreen = () => {
   const [name, setName] = useState("");
@@ -19,6 +20,8 @@ const ProfileScreen = () => {
 
   const { userInfo } = useSelector((state) => state.auth);
 
+  const [updateUser, { isLoading }] = useUpdateUserMutation();
+
   useEffect(() => {
     setName(userInfo.name);
     setEmail(userInfo.email);
@@ -29,7 +32,18 @@ const ProfileScreen = () => {
     if (password !== confirmPassword) {
       toast.error("Password do not match");
     } else {
-      console.log("submit");
+      try {
+        const res = await updateUser({
+          _id: userInfo._id,
+          name,
+          email,
+          password,
+        }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        toast.success("Profile Updated");
+      } catch (error) {
+        toast.error(err?.data?.message || err.error);
+      }
     }
   };
 
@@ -69,7 +83,7 @@ const ProfileScreen = () => {
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
         </Form.Group>
-
+        {isLoading && <Loader />}
         <Button type="submit" variant="primary" className="mt-3">
           Update
         </Button>
--- src/slice/userApiSlice.js
+++ src/slice/userApiSlice.js
@@ -18,6 +18,13 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
     logout: builder.mutation({
       query: () => ({
         url: `${USERS_URL}/logout`,
@@ -27,5 +34,9 @@ export const usersApiSlice = apiSlice.injectEndpoints({
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation } =
-  usersApiSlice;
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useUpdateUserMutation,
+} = usersApiSlice;
